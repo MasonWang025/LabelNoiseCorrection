758aea4546a1ee553c80b7a26ab03c4832588eec
Mason <37131477+MasonWang025@users.noreply.github.com>
2021-07-16 09:26:44 -0700

    update baseline bash script

--- RunBaselines.sh
+++ RunBaselines.sh
@@ -6,11 +6,11 @@
 # for noise levels 0, 20, 50, 80 (CIFAR-10)
 # reproducing M-DYR-H row in Table 2
 
-tensorboard --logdir=runs &
+# tensorboard --logdir=runs &
 
 for i in 0 20 50 80
 do
-    echo "\n\tM-DYR-H WITH NOISE LEVEL OF $i\n" 
+    printf "\n\tM-DYR-H WITH NOISE LEVEL OF $i\n\n" 
     python3 train.py --Mixup 'Static' --BootBeta 'Hard' --experiment-name 'M-DYR-H' \
 	    --epochs 300 --M 100 250 --noise-level $i --reg-term 1.0 --dataset CIFAR10 --root-dir "datasets/cifar10"
 done
